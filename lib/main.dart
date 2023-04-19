5e4c0107ae8f9f709cbf066d91679f17ec2540e2
Petri Riihikallio <petri.riihikallio@metis.fi>
2023-04-19 23:56:51 +0300

    Cogwheel

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,84 @@
 import 'package:flutter/material.dart';
-import 'package:material_color_generator/material_color_generator.dart';
 
 void main() {
-  runApp(MainApp());
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  MainApp({super.key});
+  const MainApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          brightness: Brightness.light,
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color(0xFFE66A63),
+              primary: const Color(0xFFE66A63),
+              secondary: const Color(0xFFD29DAC),
+              tertiary: const Color(0xFFF9DEDC)),
+          textTheme:
+              const TextTheme(bodyMedium: TextStyle(color: Colors.purple)),
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text("Select restaurant"),
+            ),
+            body: Body(),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {},
+              backgroundColor: const Color(0xFFD29DAC),
+              child: const Icon(Icons.settings),
+            )));
+  }
+}
+
+class Body extends StatelessWidget {
+  Body({super.key});
   final List<String> restaurants = ["YOLO", "Buffa", "Curry", "LAB", "Street"];
 
   @override
   Widget build(BuildContext context) {
-    final List<RestaurantCard> cards =
-        restaurants.map((n) => RestaurantCard(n)).toList();
-    return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: generateMaterialColor(color: Color(0xFFE66A63)),
-        textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.purple)),
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Select restaurant"),
-        ),
-        body: Center(
-          child: Container(child: Column(children: cards)),
-        ),
-      ),
+    final List<RestaurantBtn> btns =
+        restaurants.map((n) => RestaurantBtn(n)).toList();
+
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Container(
+          decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.secondary,
+              borderRadius: BorderRadius.circular(12)),
+          constraints: BoxConstraints.loose(Size(500, restaurants.length * 80)),
+          margin: const EdgeInsets.all(50),
+          padding: const EdgeInsets.all(20),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: btns)),
     );
   }
 }
 
-class RestaurantCard extends StatelessWidget {
+class RestaurantBtn extends StatelessWidget {
   final String name;
 
-  const RestaurantCard(this.name, [Key? key]) : super(key: key);
+  const RestaurantBtn(this.name, [Key? key]) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: 150,
+      height: 60,
       child: ElevatedButton(
         onPressed: () => {},
-        style:
-            ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
-        child: Text(name),
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.white,
+            textStyle:
+                const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            foregroundColor: Colors.black),
+        child: Row(children: [
+          Expanded(child: Text(name)),
+          const Icon(Icons.star_border)
+        ]),
       ),
     );
   }
