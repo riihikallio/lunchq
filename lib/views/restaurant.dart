3f846cb06daaf8595eecf9120a95bbbac5e2149f
Petri Riihikallio <petri.riihikallio@metis.fi>
2023-04-21 14:21:29 +0300

    Order total works

--- lib/views/restaurant.dart
+++ lib/views/restaurant.dart
@@ -20,8 +20,21 @@ class Restaurant extends StatelessWidget {
   }
 }
 
-class _Body extends StatelessWidget {
+class _Body extends StatefulWidget {
   const _Body({super.key});
+
+  @override
+  State<_Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<_Body> {
+  double total = 0.0;
+
+  void updateTotal(double change) {
+    setState(() {
+      total += change;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,42 +46,49 @@ class _Body extends StatelessWidget {
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          children: const [
-            Menu(),
-            Pickup(),
-            Order(),
+          children: [
+            Menu(onChange: updateTotal),
+            const Pickup(),
+            Order(total: total),
           ],
         ));
   }
 }
 
-class Entry {
+class _MenuItem {
   String name;
   double price;
-  bool isChecked;
-  Entry(this.name, this.price, {this.isChecked = false});
+  bool isChecked = false;
+  _MenuItem(this.name, this.price);
 }
 
 class Menu extends StatefulWidget {
-  const Menu({super.key});
+  final Function onChange;
+
+  const Menu({
+    Key? key,
+    required this.onChange,
+  }) : super(key: key);
 
   @override
-  State<Menu> createState() => _MenuState();
+  State<Menu> createState() => _MenuState(onChange);
 }
 
 class _MenuState extends State<Menu> {
-  final entries = [
-    Entry("Lentil Soup", 1.50),
-    Entry("Tofu Salad", 2.50),
-    Entry("Salmon", 4.50),
-    Entry("Bacon", 3.50)
+  final Function onChange;
+  _MenuState(this.onChange);
+
+  final List<_MenuItem> entries = [
+    _MenuItem("Lentil Soup", 1.50),
+    _MenuItem("Tofu Salad", 2.50),
+    _MenuItem("Salmon", 4.50),
+    _MenuItem("Bacon", 3.50)
   ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
         margin: const EdgeInsets.all(20),
-        //padding: const EdgeInsets.all(20),
         decoration: BoxDecoration(
             color: Colors.white, borderRadius: BorderRadius.circular(20)),
         height: 300,
@@ -81,13 +101,16 @@ class _MenuState extends State<Menu> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(entries[index].name),
-                    Text(NumberFormat("##0.00 €", "fi_FI")
+                    Text(NumberFormat.simpleCurrency(
+                            decimalDigits: 2, locale: "fi_FI")
                         .format(entries[index].price))
                   ]),
               value: entries[index].isChecked,
               onChanged: (bool? value) {
                 setState(() {
                   entries[index].isChecked = value!;
+                  onChange(
+                      value ? entries[index].price : -entries[index].price);
                 });
               },
             );
@@ -98,36 +121,62 @@ class _MenuState extends State<Menu> {
   }
 }
 
-class Pickup extends StatelessWidget {
+class Pickup extends StatefulWidget {
   const Pickup({super.key});
 
   @override
+  State<Pickup> createState() => _PickupState();
+}
+
+class _PickupState extends State<Pickup> {
+  TimeOfDay pickup = TimeOfDay(
+      hour: (TimeOfDay.now().hour + 1) % 24, minute: TimeOfDay.now().minute);
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: const EdgeInsets.all(20),
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.secondary,
-            borderRadius: BorderRadius.circular(20)),
-        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          const Text("Pick-up"),
-          ElevatedButton(
-              onPressed: () async {
-                TimeOfDay? pickup = await showTimePicker(
-                  context: context,
-                  initialTime: TimeOfDay.now(),
-                );
-              },
-              child: const Text(
-                "SetTime",
-                textAlign: TextAlign.center,
-              ))
-        ]));
+    return Align(
+        alignment: Alignment.centerLeft,
+        child: Container(
+            margin: const EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
+            width: 230,
+            decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.circular(20)),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("Pick-up"),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: const StadiumBorder(),
+                          padding: const EdgeInsets.all(20)),
+                      onPressed: () async {
+                        TimeOfDay? picked = await showTimePicker(
+                          context: context,
+                          initialTime: pickup,
+                        );
+                        setState(() {
+                          pickup = picked ?? pickup;
+                        });
+                      },
+                      child: Text(
+                        MaterialLocalizations.of(context).formatTimeOfDay(
+                            pickup,
+                            alwaysUse24HourFormat: true),
+                        textAlign: TextAlign.center,
+                      ))
+                ])));
   }
 }
 
 class Order extends StatelessWidget {
-  const Order({super.key});
+  final double total;
+
+  const Order({
+    Key? key,
+    required this.total,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -137,10 +186,15 @@ class Order extends StatelessWidget {
         decoration: BoxDecoration(
             color: Theme.of(context).colorScheme.secondary,
             borderRadius: BorderRadius.circular(20)),
-        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          const Text("Total 6,20€"),
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Text("Total " +
+              NumberFormat.simpleCurrency(decimalDigits: 2, locale: "fi_FI")
+                  .format(total)),
           ElevatedButton(
-              style: ElevatedButton.styleFrom(shape: const StadiumBorder()),
+              style: ElevatedButton.styleFrom(
+                  shape: const StadiumBorder(),
+                  padding: const EdgeInsets.all(20)),
               onPressed: () {},
               child: const Text("Order"))
         ]));
